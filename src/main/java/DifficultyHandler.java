2a9f3e96f0be5077b7abf59ce6848c9e307c847f
SaKaToSh <anton.sakovsky@gmail.com>
2021-12-12 22:52:07 +0300

    added DifficultyHandler docs

--- src/main/java/DifficultyHandler.java
+++ src/main/java/DifficultyHandler.java
@@ -1,10 +1,24 @@
+/**
+ * this class is responsible for setting and getting the current difficulty
+ */
 public class DifficultyHandler {
+    /**
+     * current difficulty
+     */
     private static Difficulty difficulty = Difficulty.Medium;
 
+    /**
+     * this method sets new difficulty
+     * @param newDifficulty just a new difficulty that will be set
+     */
     public static void setCurrentDifficulty(Difficulty newDifficulty){
         difficulty = newDifficulty;
     }
 
+    /**
+     * this method returns current difficulty
+     * @return return current difficulty
+     */
     public static Difficulty getCurrentDifficulty(){
        return difficulty;
     }
