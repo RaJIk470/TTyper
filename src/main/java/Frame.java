014ca688189083beed57d9ee4964d9b3b8ad099f
SaKaToSh <anton.sakovsky@gmail.com>
2021-12-12 23:00:15 +0300

    added Frame docs

--- src/main/java/Frame.java
+++ src/main/java/Frame.java
@@ -1,13 +1,33 @@
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * This class is responsible for main window with text to type
+ */
 public class Frame extends JFrame{
+    /**
+     * instance for Frame class
+     */
     private static Frame instance = new Frame();
+    /**
+     * size of the layout for main Frame with coordinate X
+     */
     int layoutSizeX = 1;
+    /**
+     * size of the layout for main Frame with coordinate Y
+     */
     int layoutSizeY = 1;
 
+    /**
+     * panels for location main label on it
+     */
     JPanel[][] layoutPanels = new JPanel[layoutSizeY][layoutSizeX];
 
+    /**
+     * Constructor for Frame class
+     * the constructor generates layout and put main label on it
+     * also calls methods for generating words
+     */
     Frame(){
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(new GridLayout(layoutSizeY, layoutSizeX));
@@ -23,10 +43,13 @@ public class Frame extends JFrame{
 
         this.addKeyListener(TypingListener.getInstance());
         layoutPanels[0][0].add(WordsLabel.getInstance());
-        WordsHandler.getInstance().setCurrentWords(WordsGenerator.getInstance().generateRandomWords(40, DifficultyHandler.getCurrentDifficulty()));
+        WordsHandler.getInstance().setCurrentWords(WordsGenerator.getInstance().generateRandomWords(30, DifficultyHandler.getCurrentDifficulty()));
         WordsLabel.getInstance().updateText(WordsHandler.getInstance().formatWords());
     }
 
+    /**
+     * This method resets words in main label by calling corresponding methods
+     */
     public void resetWords(){
         int wordsCount;
         switch (WordsCountHandler.getCurrentWordsCount()){
@@ -50,7 +73,11 @@ public class Frame extends JFrame{
         AppTimer.getInstance().reset();
     }
 
-    public static  Frame getInstance(){
+    /**
+     * This method returns instance of Frame class
+     * @return return instance of Frame class
+     */
+    public static Frame getInstance(){
         return instance;
     }
 }
