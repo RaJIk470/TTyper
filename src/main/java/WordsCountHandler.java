72da5b0ad1bc5a7ab2890d576400682be1f11ec7
Korol Nikita <korol634921777@gmail.com>
2021-12-14 16:09:19 +0300

    Added WordsCountHandler docs

--- src/main/java/WordsCountHandler.java
+++ src/main/java/WordsCountHandler.java
@@ -1,10 +1,24 @@
+/**
+ * words count handler class, the class is responsible for setting and getting words count
+ */
 public class WordsCountHandler {
-    private static WordsCount wordsCount = WordsCount.FifteenWords;
+    /**
+     * instance of WordsCountHandler class
+     */
+    private static WordsCount wordsCount = WordsCount.ThirtyWords;
 
+    /**
+     * this method sets a new words count
+     * @param newWordsCount new words count to replace old one
+     */
     public static void setWordsCount(WordsCount newWordsCount) {
         wordsCount = newWordsCount;
     }
 
+    /**
+     * this method returns current words count
+     * @return return current words count
+     */
     public static WordsCount getCurrentWordsCount() {
         return wordsCount;
     }
