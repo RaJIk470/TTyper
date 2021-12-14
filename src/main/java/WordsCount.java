b058e8e7a083e3c75ea3276cbdd964dde572f4ba
Korol Nikita <korol634921777@gmail.com>
2021-12-14 16:09:50 +0300

    Added WordsCount docs

--- src/main/java/WordsCount.java
+++ src/main/java/WordsCount.java
@@ -1,6 +1,21 @@
+/**
+ * Enum class declares the words count: Fifteen words, thirty words, forty-five words, sixty words
+ */
 public enum WordsCount {
+    /**
+     * for generating 15 words
+     */
     FifteenWords,
+    /**
+     * for generating 30 words
+     */
     ThirtyWords,
+    /**
+     * for generating 45 words
+     */
     FortyFiveWords,
+    /**
+     * for generating 60 words
+     */
     SixtyWords
 }
