45651ec89c78e37178f85f7002834898333452e4
SaKaToSh <anton.sakovsky@gmail.com>
2021-12-14 16:39:33 +0300

    added WordsOptions docs

--- src/main/java/WordsOptions.java
+++ src/main/java/WordsOptions.java
@@ -1,6 +1,21 @@
+/**
+ * Enum class declares the words options: Commas, UpperSymbols, Digits and SpecialSymbols
+ */
 public enum WordsOptions {
+    /**
+     * option for commas after words
+     */
     Commas,
+    /**
+     * option for upper symbols
+     */
     UpperSymbols,
+    /**
+     * option for digits in words
+     */
     Digits,
+    /**
+     * option for special symbols in words
+     */
     SpecialSymbols
 }
