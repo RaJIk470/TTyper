2709fdd719eacbc5206818782b32ae350a5c75b1
Dmitry Stoyanov <you@examdmitryrajik@gmail.com>
2021-12-14 14:46:49 +0300

    added WordsOptionsPanel docs

--- src/main/java/WordsOptionsPanel.java
+++ src/main/java/WordsOptionsPanel.java
@@ -3,9 +3,18 @@ import java.awt.*;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
+/**
+ * words options panel class, the class is responding for storing buttons of words options
+ */
 public class WordsOptionsPanel extends JPanel implements ItemListener{
+    /**
+     * instance of WordsOptionsPanel class
+     */
     private static WordsOptionsPanel instance = new WordsOptionsPanel();
 
+    /**
+     * construction of WordsOptionPanel class, the constructor defines check boxes of words options
+     */
     WordsOptionsPanel(){
         JCheckBox commasBox = new JCheckBox("Commas");
         JCheckBox specialSymbolsBox = new JCheckBox("Special symbols");
@@ -27,6 +36,10 @@ public class WordsOptionsPanel extends JPanel implements ItemListener{
         this.setBackground(new Color(44, 44, 44));
     }
 
+    /**
+     * action of check boxes that sets necessary option and resets the words
+     * @param e ItemEvent parameter that contains the name of the button
+     */
     @Override
     public void itemStateChanged(ItemEvent e){
         JCheckBox check = (JCheckBox)e.getSource();
@@ -55,9 +68,14 @@ public class WordsOptionsPanel extends JPanel implements ItemListener{
             else
                 WordsOptionsHandler.unsetUpperSymbols();
         }
+        Frame.getInstance().resetWords();
     }
 
 
+    /**
+     * the method returns the instance of the class
+     * @return return the instance of the class
+     */
     public static WordsOptionsPanel getInstance(){
         return instance;
     }
