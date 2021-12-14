faa228caf311d94bff8a4d38b1ce0ef11fb6850b
SaKaToSh <anton.sakovsky@gmail.com>
2021-12-14 16:26:24 +0300

    added WordsCountPanel docs

--- src/main/java/WordsCountPanel.java
+++ src/main/java/WordsCountPanel.java
@@ -3,13 +3,23 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * words count panel class, the class is responsible for panel that stores radio buttons for changing words count
+ */
 public class WordsCountPanel extends JPanel implements ActionListener {
+    /**
+     * instance of WordsCountPanel class
+     */
     private static WordsCountPanel instance = new WordsCountPanel();
 
+    /**
+     * constructor of WordsCountPanel class, the constructor defines buttons for changing words count
+     */
     WordsCountPanel(){
         JRadioButton easyButton = new JRadioButton("15");
-        easyButton.setSelected(true);
+        //easyButton.setSelected(true);
         JRadioButton mediumButton = new JRadioButton("30");
+        mediumButton.setSelected(true);
         JRadioButton hardButton = new JRadioButton("45");
         JRadioButton insaneButton = new JRadioButton("60");
 
@@ -30,9 +40,13 @@ public class WordsCountPanel extends JPanel implements ActionListener {
         mediumButton.addActionListener(this);
         hardButton.addActionListener(this);
         insaneButton.addActionListener(this);
-        this.setBackground(new Color(44, 44, 44));
     }
 
+    /**
+     * action of the buttons
+     * when button presses words count changes and words in main label resets
+     * @param e ActionEvent parameter that stores name of the buttons
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getActionCommand().equals("15")) {
@@ -54,6 +68,10 @@ public class WordsCountPanel extends JPanel implements ActionListener {
 
     }
 
+    /**
+     * this method returns the instance of the class
+     * @return return the instance of the class
+     */
     public static WordsCountPanel getInstance(){
         return instance;
     }
